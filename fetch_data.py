28fcccf2a7616ac58ca7f62e72ed55d40d36ff3f
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-18 13:34:36 +0200

    added fetch_data script

---
+++ fetch_data.py
@@ -0,0 +1,13 @@
+import os
+import json
+from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
+
+
+tr_user = os.environ.get("TRANSKRIBUS_USER")
+tr_pw = os.environ.get("TRANSKRIBUS_PASSWORD")
+client = ACDHTranskribusUtils(user=tr_user, password=tr_pw)
+
+data = client.create_status_report("wrz__", transcription_threshold=20)
+
+with open("data.json", "w") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
