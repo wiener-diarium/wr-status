58f95d784db04cf65e8a6e74f1625f551934be49
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-20 15:00:23 +0200

    minor

--- fetch_data.py
+++ fetch_data.py
@@ -3,8 +3,8 @@ import json
 from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
 
 
-tr_user = os.environ.get("TRANSKRIBUS_USER")
-tr_pw = os.environ.get("TRANSKRIBUS_PASSWORD")
+tr_user = os.environ.get("TR_USER")
+tr_pw = os.environ.get("TR_PW")
 client = ACDHTranskribusUtils(user=tr_user, password=tr_pw)
 
 data = client.create_status_report("wrz__", transcription_threshold=20)
